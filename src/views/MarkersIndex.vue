03598a63a3661b0d4be82e4d0444bebb6efd7f66
tommy3wood <thomaswcurran1014@gmail.com>
2020-02-28 15:17:31 -0600

    initial setup

---
+++ src/views/MarkersIndex.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="index">
+    <h1>{{message}}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function() {
+      return {
+        message: "This is our poop app!"
+      };
+    },
+    created: function() {},
+    methods: {}
+  }
+</script>
